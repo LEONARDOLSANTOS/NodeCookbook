9aa1d513c0cec01cef30779c18b2a6893850e685
Leonardo Santos <leonardo.ssantos@mpt.mp.br>
2023-01-17 11:29:57 -0300

    Refactoring

--- interfacing-with-io/greeting.js
+++ interfacing-with-io/greeting.js
@@ -1,8 +1,10 @@
+// Listens for the user input
 process.stdin.on("data", (data) => {
     const name = data.toString().trim().toUpperCase();
     if (name !== "") {
-        process.stdout.write(`Hello ${name}, wellcome to NODEJS World!!`);
+        process.stdout.write(`Hello ${name}, wellcome to NODEJS World!!\r\n`);
     } else {
-        process.stderr.write(`Realy!!!, nothing to say?`);
+        process.stderr.write(`Realy!!!, nothing to say?\r\n`);
+        process.exit();
     }
 });
